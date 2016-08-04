dfe964f5f1de4131ce660512dc51e0bc75b08cbb
privod <bespalov@krsk.ustech.ru>
2016-08-04 22:51:57 +0700

    Заложен класс для работы с текстом с разделителем

---
+++ septext/core.py
@@ -0,0 +1,29 @@
+
+class SepText:
+
+  def __init__(self, sep, text=None, file=None):
+    self._sep = sep
+    # if text != None:
+      # self._data = self.
+      # open(fname, mode)
+
+  def __del__(self):
+      self._file.close()
+
+  # def _get
+  def get_strlst(self):
+    return self._file.readlines()
+
+  def get_array(self, sep=None):
+    return [str.split(sep) for str in self.get_strlst()]
+
+  def get_dict(self, sep=None):
+    arr = self.get_array(sep)
+    titles = arr[0]
+    dict = []
+    for rec in arr[1:]:
+      dict.append({titles[fnum]: fval for fnum, fval in enumerate(rec, 0)})
+        #~ print titles[fnum], ": ", fval
+        #~ raw_input()
+        #~ dict.append({titles[fnum]: fval})
+    return dict
